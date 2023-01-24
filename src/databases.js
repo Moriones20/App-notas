f7e63a36e24d30e019ccb11061ac4becdd70d9bd
Moriones20 <moriones06082001@gmail.com>
2023-01-24 11:57:45 -0500

    First commit

---
+++ src/databases.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const {APP_NOTAS_MONGODB_HOST, APP_NOTAS_MONGODB_DATABASE} = process.env;
+const MONGODB_URI = APP_NOTAS_MONGODB_HOST + '/' + APP_NOTAS_MONGODB_DATABASE;
+
+mongoose.set('strictQuery', false)
+mongoose.connect(MONGODB_URI, {
+})
+    .then(db => console.log('Database is connected'))
+    .catch(err => console.log(err));
---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+//Initializations
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+
+//Global variables
+
+//Routes
+app.use(require('./routes/index.routes'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
