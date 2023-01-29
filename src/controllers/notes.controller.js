e86761cd2ed398c44fcc84d3cb9b0e83f9bc9838
Moriones20 <moriones06082001@gmail.com>
2023-01-29 18:30:54 -0500

    messages with Connect-flash

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -9,6 +9,7 @@ notesCtrl.createNewNote = async (req, res) => {
   const { title, description } = req.body;
   const newNote = new Note({ title, description });
   await newNote.save();
+  req.flash("success_msg", "Note Added successfully");
   res.redirect("/notes");
 };
 
@@ -22,14 +23,16 @@ notesCtrl.renderEditForm = async (req, res) => {
   res.render("notes/edit-note", { note });
 };
 
-notesCtrl.updateNote = async(req, res) => {
+notesCtrl.updateNote = async (req, res) => {
   const { title, description } = req.body;
-  await Note.findByIdAndUpdate(req.params.id, {title, description});
+  await Note.findByIdAndUpdate(req.params.id, { title, description });
+  req.flash("success_msg", "Note Updated Successfully");
   res.redirect("/notes");
 };
 
 notesCtrl.deleteNote = async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Note Deleted successfully");
   res.redirect("/notes");
 };
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const exphbs = require("express-handlebars");
 const path = require("path");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 //Initializations
 const app = express();
@@ -22,11 +24,23 @@ app.engine(
 app.set("view engine", "hbs");
 
 //middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 //Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  next();
+});
 
 //Routes
 app.use(require("./routes/index.routes"));
