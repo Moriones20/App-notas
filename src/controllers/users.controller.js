4ed452e1014e63d8ace8b07e82b8b2f0ef749d2d
Moriones20 <moriones06082001@gmail.com>
2023-02-04 19:02:15 -0500

    SignUp

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,11 +1,41 @@
+const { text } = require("express");
+
 const usersCtrl = {};
+const User = require('../models/User');
 
 usersCtrl.renderSignUpForm = (req, res) => {
   res.render("users/signup");
 };
 
-usersCtrl.signup = (req, res) => {
-  res.send("signup");
+usersCtrl.signup = async (req, res) => {
+  const errors = [];
+  const { name, email, password, confirm_password } = req.body;
+  if (password != confirm_password) {
+    errors.push({text: "Password don't match"});
+  }
+  if (password.length < 4) {
+    errors.push({text: "Password must be at least 4 characters."});
+  }
+  if (errors.length > 0) {
+    res.render('users/signup', {
+      errors,
+      name,
+      email
+    })
+  }
+  else {
+    const emailUser = await User.findOne({email: email})
+    if (emailUser) {
+      req.flash('error_msg', 'The email is already in use');
+      res.redirect('/users/signup');
+    } else {
+      const newUser = new User({name, email, password});
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash('success_msg', "you're registered")
+      res.redirect('/users/signin')
+    }
+  }
 };
 
 usersCtrl.renderSignInForm = (req, res) => {
