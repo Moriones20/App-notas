6540d2f9c0a1f68b9f72bb3021c6fbfdb0a92aae
Moriones20 <moriones06082001@gmail.com>
2023-02-04 21:57:04 -0500

    Passport & bcrypt

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,8 @@
 const { text } = require("express");
 
 const usersCtrl = {};
-const User = require('../models/User');
+const passport = require("passport");
+const User = require("../models/User");
 
 usersCtrl.renderSignUpForm = (req, res) => {
   res.render("users/signup");
@@ -11,29 +12,28 @@ usersCtrl.signup = async (req, res) => {
   const errors = [];
   const { name, email, password, confirm_password } = req.body;
   if (password != confirm_password) {
-    errors.push({text: "Password don't match"});
+    errors.push({ text: "Password don't match" });
   }
   if (password.length < 4) {
-    errors.push({text: "Password must be at least 4 characters."});
+    errors.push({ text: "Password must be at least 4 characters." });
   }
   if (errors.length > 0) {
-    res.render('users/signup', {
+    res.render("users/signup", {
       errors,
       name,
-      email
-    })
-  }
-  else {
-    const emailUser = await User.findOne({email: email})
+      email,
+    });
+  } else {
+    const emailUser = await User.findOne({ email: email });
     if (emailUser) {
-      req.flash('error_msg', 'The email is already in use');
-      res.redirect('/users/signup');
+      req.flash("error_msg", "The email is already in use");
+      res.redirect("/users/signup");
     } else {
-      const newUser = new User({name, email, password});
+      const newUser = new User({ name, email, password });
       newUser.password = await newUser.encryptPassword(password);
       await newUser.save();
-      req.flash('success_msg', "you're registered")
-      res.redirect('/users/signin')
+      req.flash("success_msg", "you're registered");
+      res.redirect("/users/signin");
     }
   }
 };
@@ -42,12 +42,20 @@ usersCtrl.renderSignInForm = (req, res) => {
   res.render("users/signin");
 };
 
-usersCtrl.signin = (req, res) => {
-  res.send("signin");
-};
+usersCtrl.signin = passport.authenticate("local", {
+  failureRedirect: "/users/signin",
+  successRedirect: "/notes",
+  failureFlash: true,
+});
 
-usersCtrl.logout = (req, res) => {
-  res.send("logout");
+usersCtrl.logout = (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success_msg', "You're logged out now");
+    res.redirect("/users/signin");
+  });
 };
 
 module.exports = usersCtrl;
--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const UserSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   {
@@ -17,7 +17,7 @@ UserSchema.methods.encryptPassword = async (password) => {
   return await bcrypt.hash(password, salt);
 };
 
-UserSchema.method.matchPassword = async function (Password) {
+UserSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
