5c035fef1cae27d7970c140f61befc2f1024eaf3
Moriones20 <moriones06082001@gmail.com>
2023-02-05 20:46:30 -0500

    Protect Express routes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -10,18 +10,20 @@ const {
   deleteNote,
 } = require("../controllers/notes.controller");
 
+const { isAuthenticated } = require('../helpers/auth')
+
 //NEW NOTES
-router.get("/notes/add", renderNoteForm);
-router.post("/notes/new-notes", createNewNote);
+router.get("/notes/add", isAuthenticated, renderNoteForm);
+router.post("/notes/new-notes", isAuthenticated, createNewNote);
 
 //GET ALL NOTE
-router.get("/notes", renderNotes);
+router.get("/notes", isAuthenticated, renderNotes);
 
 //EDIT NOTES
-router.get("/notes/edit/:id", renderEditForm);
-router.put("/notes/edit/:id", updateNote);
+router.get("/notes/edit/:id", isAuthenticated, renderEditForm);
+router.put("/notes/edit/:id", isAuthenticated, updateNote);
 
 //DELETE NOTE
-router.delete("/notes/delete/:id", deleteNote);
+router.delete("/notes/delete/:id", isAuthenticated, deleteNote);
 
 module.exports = router;
